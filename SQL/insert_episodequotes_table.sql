983f6970a9ff96aad1e392cb816e8d123367e26d
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 11:50:56 -0600

    inserting a few values into the tables for testing the api soon

---
+++ SQL/insert_episodequotes_table.sql
@@ -0,0 +1,17 @@
+-- The Contest (EpisodeId = 1)
+INSERT INTO EpisodeQuotes (EpisodeId, Quote, Character)
+VALUES
+(1, 'But are you still master of your domain?', 'Jerry'),
+(1, 'I''m out!', 'George');
+
+-- The Soup Nazi (EpisodeId = 2)
+INSERT INTO EpisodeQuotes (EpisodeId, Quote, Character)
+VALUES
+(2, 'No soup for you!', 'Soup Nazi'),
+(2, 'Do you think I should wear the ribbon?', 'Kramer');
+
+-- The Puffy Shirt (EpisodeId = 3)
+INSERT INTO EpisodeQuotes (EpisodeId, Quote, Character)
+VALUES
+(3, 'But I don''t want to be a pirate!', 'Jerry'),
+(3, 'You''re gonna be the first pirate!', 'Kramer');
---
+++ SQL/insert_episodes_table.sql
@@ -0,0 +1,7 @@
+USE SeinfeldDB;
+
+INSERT INTO Episodes (Title, Season, EpisodeNumber, AirDate)
+VALUES
+('The Contest', '4', '11', '1992-11-18'),
+('The Soup Nazi', '7', '6', '1995-11-02'),
+('The Puffy Shirt', '5', '2', '1993-09-23');
