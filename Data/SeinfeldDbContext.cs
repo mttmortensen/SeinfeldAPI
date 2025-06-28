75c60123f8d8a4f110a3def22217099809d84f9a
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 12:11:08 -0600

    simple logic for creating a c# model from the sql table

--- Data/SeinfeldDbContext.cs
+++ Data/SeinfeldDbContext.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SeinfeldAPI.Models;
 
 namespace SeinfeldAPI.Data
 {
     public class SeinfeldDbContext : DbContext
     {
+        // Constructor lets EF use options like the connection string
+        public SeinfeldDbContext(DbContextOptions<SeinfeldDbContext> options)
+            : base(options)
+        { }
+
+        // This tells EF: "Create a table names Episodes using the Episode Model"
+        public DbSet<Episode> Episodes { get; set; }
+
+        // Same thing here: This maps the EpisodeQuotes model to the SQL Table
+        public DbSet<EpisodeQuotes> EpisodeQuotes { get; set; }
 
     }
 }
