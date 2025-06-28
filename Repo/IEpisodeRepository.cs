ff547b0de2bbdd69cf6018ab4f061782a24dce2d
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 12:55:47 -0600

    interface headers

--- Repo/IEpisodeRepository.cs
+++ Repo/IEpisodeRepository.cs
@@ -1,7 +1,18 @@
-﻿namespace SeinfeldAPI.Repo
+﻿using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Repo
 {
     public interface IEpisodeRepository
     {
+        List<Episode> GetAllEpisodes();
+        Episode? GetEpisodeById(int id);
+
+        // These methods would be used right away
+        // But just declaring them for now
+        void AddEpisode(Episode episode);
+        void UpdateEpisode(Episode episode);
+        void DeleteEpisode(int id);
+        bool SaveChanges();
 
     }
 }
