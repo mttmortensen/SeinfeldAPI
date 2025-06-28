936cd93b68ef8d8343b5dc39bb465c826abe355a
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 13:58:02 -0600

    construct and get all logic setup

--- Repo/EpisodeRepoistory.cs
+++ Repo/EpisodeRepoistory.cs
@@ -9,5 +9,19 @@ namespace SeinfeldAPI.Repo
     {
         // Holds a reference to the db for querying 
         private readonly SeinfeldDbContext _context;
+
+        // Constructor: sets the _context when the repo is created 
+        public EpisodeRepoistory(SeinfeldDbContext context) 
+        {
+            _context = context;
+        }
+
+        // Returns all Episodes, including their quotes (EpisodeQuotes)
+        public List<Episode> GetAllEpisodes()
+        {
+            return _context.Episodes
+                .Include(e => e.Quotes)
+                .ToList();
+        }
     }
 }
