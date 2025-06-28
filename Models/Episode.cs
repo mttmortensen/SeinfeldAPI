49686e8221015ffa2b6a9a93ca706cf7b357ec18
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 10:23:14 -0600

    creating Episode model

---
+++ Models/Episode.cs
@@ -0,0 +1,11 @@
+﻿namespace SeinfeldAPI.Models
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Season { get; set; }
+        public string EpisodeNumber { get; set; }
+        public string AirDate { get; set; }
+    }
+}
