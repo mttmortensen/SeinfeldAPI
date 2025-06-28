fcf39a3ae9d2782ea8b8b6d0950cf27eafb178f1
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-28 13:00:32 -0600

    starting on repo logic for episode

---
+++ Repo/EpisodeRepoistory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeinfeldAPI.Data;
+using SeinfeldAPI.Interfaces;
+using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Repo
+{
+    public class EpisodeRepoistory : IEpisodeRepository
+    {
+        // Holds a reference to the db for querying 
+        private readonly SeinfeldDbContext _context;
+    }
+}
