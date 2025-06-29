9049ccd8fa4cc3aa009e7ad27cb623c412ae964f
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-29 14:05:23 -0600

    now doing an interface for episode quotes. Also including  method that will bring in quotes via the episode

---
+++ Interfaces/IEpisodeQuotesRepository.cs
@@ -0,0 +1,20 @@
+﻿using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Interfaces
+{
+    public interface IEpisodeQuotesRepository
+    {
+
+        List<EpisodeQuotes> GetAllQuotes();
+        List<EpisodeQuotes> GetQuotesForEpisode(int episodeId);
+        EpisodeQuotes? GetQuoteById(int id);
+
+        // These methods wouldn't be used right away
+        // But just declaring them for now
+
+        void AddQuote(EpisodeQuotes quote);
+        void UpdateQuote(EpisodeQuotes quote);
+        void DeleteQuote(int id);
+        bool SaveChanges();
+    }
+}
