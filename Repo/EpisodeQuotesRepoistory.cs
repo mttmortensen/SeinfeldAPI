c508cefadb9d4ae58b5d9e59ea79e09724ece867
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-29 22:10:34 -0600

    all get logic added for episodequotesx

--- Repo/EpisodeQuotesRepoistory.cs
+++ Repo/EpisodeQuotesRepoistory.cs
@@ -1,5 +1,7 @@
-﻿using SeinfeldAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SeinfeldAPI.Data;
 using SeinfeldAPI.Interfaces;
+using SeinfeldAPI.Models;
 
 namespace SeinfeldAPI.Repo
 {
@@ -13,5 +15,32 @@ namespace SeinfeldAPI.Repo
         {
             _context = context;
         }
+
+        // Return all quotes in the db (regardless of episode)
+        public List<EpisodeQuotes> GetAllQuotes()
+        {
+            return _context.EpisodeQuotes
+                .Include(q => q.Episode) // This will bring in all the episode date with it, might wanna trim this down later
+                .ToList();
+        }
+
+        // Return all quotes for a specific episode
+        public List<EpisodeQuotes> GetQuotesForEpisode(int episodeId) 
+        {
+            return _context.EpisodeQuotes
+                .Where(q => q.EpisodeId == episodeId)
+                .ToList();
+        }
+
+        // Return a single quote by it's Id
+        public EpisodeQuotes? GetQuoteById(int id) 
+        {
+            return _context.EpisodeQuotes
+                .Include(q => q.Episode)
+                .FirstOrDefault(q => q.Id == id);
+        }
+
+
+
     }
 }
