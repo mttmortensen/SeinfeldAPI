44ae21f4469e5565cc40953e955aa5ba109c9de8
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-29 19:12:56 -0600

    basic header and construct

--- Repo/EpisodeQuotesRepoistory.cs
+++ Repo/EpisodeQuotesRepoistory.cs
@@ -1,8 +1,17 @@
-﻿using SeinfeldAPI.Interfaces;
+﻿using SeinfeldAPI.Data;
+using SeinfeldAPI.Interfaces;
 
 namespace SeinfeldAPI.Repo
 {
     public class EpisodeQuotesRepoistory: IEpisodeQuotesRepository
     {
+        // Reference to the db via the context
+        private readonly SeinfeldDbContext _context;
+
+        // Construct injecting the context
+        public EpisodeQuotesRepoistory(SeinfeldDbContext context) 
+        {
+            _context = context;
+        }
     }
 }
