099eb13df6530a9f27d916a8004a53f89ae28997
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 15:52:05 -0600

    now starting on the eq controller

---
+++ Controllers/EpisodeQuotesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeinfeldAPI.Services;
+
+namespace SeinfeldAPI.Controllers
+{
+    // Marks this class as an API controller
+    [ApiController]
+
+    // Sets base route to "api/episodequotes"
+    [Route("api/episodequotes")]
+    public class EpisodeQuotesController : ControllerBase
+    {
+        // Service that handles all quote logic
+        private readonly EpisodeQuotesService _quotesService;
+
+        // Inject the EpisodeQuotesService through constructor
+        public EpisodeQuotesController(EpisodeQuotesService quotesService)
+        {
+            _quotesService = quotesService;
+        }
+    }
+}
