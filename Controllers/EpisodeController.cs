02e1b23bae65197a4c29a85898c25820e26c1dbb
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 14:53:17 -0600

    starting on epsiode controller

---
+++ Controllers/EpisodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeinfeldAPI.Controllers
+{
+    public class EpisodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
