696ab6692fd762df9837cbd5399eda9280264330
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 14:26:45 -0600

    I'm really liking the idea of interfaces and will be using them more often

---
+++ Interfaces/IEpisodeService.cs
@@ -0,0 +1,27 @@
+﻿using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Interfaces
+{
+    public interface IEpisodeService
+    {
+        // Get all episodes
+        public void GetAllEpisodes()
+        {}
+
+        // Get a specific episode by ID
+        public void GetEpisodeById(int id)
+        {}
+
+        // Add a new episode
+        public void AddEpisode(Episode episode)
+        {}
+
+        // Update an existing episode
+        public void UpdateEpisode(Episode episode)
+        {}
+
+        // Delete an episode by ID
+        public void DeleteEpisode(int id)
+        {}
+    }
+}
