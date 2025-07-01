d7df98558e4e6d22ef891b4fc34505278d7ad293
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 15:58:47 -0600

    get req logic for eqs

--- Controllers/EpisodeQuotesController.cs
+++ Controllers/EpisodeQuotesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SeinfeldAPI.Models;
 using SeinfeldAPI.Services;
 
 namespace SeinfeldAPI.Controllers
@@ -17,6 +18,45 @@ namespace SeinfeldAPI.Controllers
         public EpisodeQuotesController(EpisodeQuotesService quotesService)
         {
             _quotesService = quotesService;
+        }
+
+        // Handles GET requests to /api/episodequotes
+        // This brings in all quotes from the db 
+        [HttpGet]
+        public ActionResult<List<EpisodeQuotes>> GetAllQuotes()
+        {
+            // Get all quotes in the system
+            List<EpisodeQuotes> quotes = _quotesService.GetAllQuotes();
+
+            // Return them with 200 OK
+            return Ok(quotes);
+        }
+
+        // Handles GET requests to /api/episodequotes/episode/{episodeId}
+        // This brings in all the quotes for a specific episode
+        [HttpGet("episode/{episodeId}")]
+        public ActionResult<List<EpisodeQuotes>> GetQuotesForEpisode(int episodeId)
+        {
+            // Get all quotes for a specific episode
+            List<EpisodeQuotes> quotes = _quotesService.GetQuotesForEpisode(episodeId);
+
+            // Return them with 200 OK
+            return Ok(quotes);
+        }
+
+        // Handles GET requests to /api/episodequotes/{id}
+        [HttpGet("{id}")]
+        public ActionResult<EpisodeQuotes> GetQuoteById(int id)
+        {
+            // Try to get the quote with this ID
+            EpisodeQuotes quote = _quotesService.GetQuoteById(id);
+
+            // If not found, return 404
+            if (quote == null)
+                return NotFound();
+
+            // Return the quote with 200 OK
+            return Ok(quote);
         }
     }
 }
