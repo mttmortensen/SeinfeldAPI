3fa68982deea2a89c6d930d99ec1b7f7ab64185a
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 14:32:59 -0600

    now adding the service layer for eq

---
+++ Services/EpisodeQuotesService.cs
@@ -0,0 +1,14 @@
+﻿using SeinfeldAPI.Repo;
+
+namespace SeinfeldAPI.Services
+{
+    public class EpisodeQuotesService
+    {
+        private readonly EpisodeQuotesRepoistory _eqrepo;
+
+        public EpisodeQuotesService(EpisodeQuotesRepoistory eqrepo) 
+        {
+            _eqrepo = eqrepo;
+        }
+    }
+}
