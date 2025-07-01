0e2515f64b5de2c413ad6eec9deda3cea71b78dc
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 14:40:50 -0600

    method contracts for interface eq

--- Interfaces/IEpisodeQuotesService.cs
+++ Interfaces/IEpisodeQuotesService.cs
@@ -1,6 +1,31 @@
-﻿namespace SeinfeldAPI.Interfaces
+﻿using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Interfaces
 {
     public interface IEpisodeQuotesService
     {
+        // Get all quotes from all episodes
+        public void GetAllQuotes()
+        {}
+
+        // Get all quotes for a specific episode
+        public void GetQuotesForEpisode(int episodeId)
+        {}
+
+        // Get a single quote by ID
+        public void GetQuoteById(int id)
+        {}
+
+        // Add a new quote (only if the episode exists)
+        public void AddQuote(EpisodeQuotes quote)
+        {}
+
+        // Update an existing quote
+        public void UpdateQuote(EpisodeQuotes quote)
+        {}
+
+        // Delete a quote by ID
+        public void DeleteQuote(int id)
+        {}
     }
 }
