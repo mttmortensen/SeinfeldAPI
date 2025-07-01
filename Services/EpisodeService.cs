d168e5ebff492c32687e14582704b763c35e684b
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 14:23:29 -0600

    adding service layer, rn v basic but will handle error handling, and any complex filtering, etc.

---
+++ Services/EpisodeService.cs
@@ -0,0 +1,27 @@
+﻿using SeinfeldAPI.Interfaces;
+using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Services
+{
+    public class EpisodeService
+    {
+        private readonly IEpisodeRepository _episodeRepo;
+
+        public EpisodeService(IEpisodeRepository episodeRepo)
+        {
+            _episodeRepo = episodeRepo;
+        }
+
+        // Get all episodes
+        public List<Episode> GetAllEpisodes()
+        {
+            return _episodeRepo.GetAllEpisodes();
+        }
+
+        // Get a specific episode by ID
+        public Episode? GetEpisodeById(int id)
+        {
+            return _episodeRepo.GetEpisodeById(id);
+        }
+    }
+}
