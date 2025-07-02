9d92d8519c5076038dcd8a15b727db575500324a
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-02 14:10:53 -0600

    I need to trim down EQ even futher for getting all episodes as it pull in un-needed data.

---
+++ Models/DTOs/EpisodeQuoteFlatDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SeinfeldAPI.Models.DTOs
+{
+    public class EpisodeQuoteFlatDto
+    {
+        public int Id { get; set; }
+        public string Quote { get; set; }
+        public string Character { get; set; }
+        public int EpisodeId { get; set; }
+    }
+}
