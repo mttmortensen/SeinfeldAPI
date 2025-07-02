e79ca2d3b963fbe5db6502bafae634b90ca2b813
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-01 18:59:37 -0600

    AiredDate needs to be a DateTime type

--- Models/Episode.cs
+++ Models/Episode.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Season { get; set; }
         public string EpisodeNumber { get; set; }
-        public string AirDate { get; set; }
+        public DateTime AirDate { get; set; }
         
         // 1 episode --> many quotes
         public List<EpisodeQuotes> Quotes { get; set; }
