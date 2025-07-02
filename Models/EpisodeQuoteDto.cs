e13aae4c676c5b03b92793b444c32fd1182e333d
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-02 08:02:32 -0600

    I needed to do a dto for eq because of how nested it can return when returning the whole episode within the EQ class

---
+++ Models/EpisodeQuoteDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SeinfeldAPI.Models
+{
+    public class EpisodeQuoteDto
+    {
+        public int Id { get; set; }
+        public string Quote { get; set; }
+        public string Character { get; set; }
+        public int EpisodeId { get; set; }
+        public string EpisodeTitle { get; set; }
+        public string EpisodeSeason { get; set; }
+    }
+}
