2948bbeb9cf9ca4c53ee1499595bdfee3b387591
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-02 13:29:18 -0600

    episode dto isn't too different from the original just now accepting the EQ dto

--- Models/DTOs/EpisodeDto.cs
+++ Models/DTOs/EpisodeDto.cs
@@ -2,5 +2,13 @@
 {
     public class EpisodeDto
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Season { get; set; }
+        public string EpisodeNumber { get; set; }
+        public DateTime AirDate { get; set; }
+
+        // Including now the simplifed EpisodeQuotes data
+        public List<EpisodeQuoteDto> Quotes { get; set; }
     }
 }
