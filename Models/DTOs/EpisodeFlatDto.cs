13d4efced79b746449b4208107c45019ab5d6e58
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-03 14:24:14 -0600

    FlatDto for Episode so it won't need to use Quotes when updating over the service layer

---
+++ Models/DTOs/EpisodeFlatDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SeinfeldAPI.Models.DTOs
+{
+    public class EpisodeFlatDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Season { get; set; }
+        public string? EpisodeNumber { get; set; }
+        public DateTime? AirDate { get; set; }
+    }
+}
