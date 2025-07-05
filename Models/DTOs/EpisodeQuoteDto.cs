d46e87b1043c7b0fe2776550b97f3d41024b43a8
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-05 11:33:22 -0600

    adding '?' to handle null as we prepare to use a function to resolve episode Id dynamically

--- Models/DTOs/EpisodeQuoteDto.cs
+++ Models/DTOs/EpisodeQuoteDto.cs
@@ -11,8 +11,8 @@
         public int Id { get; set; }
         public string Quote { get; set; }
         public string Character { get; set; }
-        public int EpisodeId { get; set; }
-        public string EpisodeTitle { get; set; }
-        public string EpisodeSeason { get; set; }
+        public int? EpisodeId { get; set; }
+        public string? EpisodeTitle { get; set; }
+        public string? EpisodeSeason { get; set; }
     }
 }
