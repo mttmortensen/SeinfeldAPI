6984141bb7243528df72cc446894f110a31fe663
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-06 00:04:28 -0600

    validation for inputs on eqs

--- Models/DTOs/QuoteCreateOrUpdateDto.cs
+++ Models/DTOs/QuoteCreateOrUpdateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace SeinfeldAPI.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeinfeldAPI.Models.DTOs
 {
     /*
      * POST /api/episodequotes
@@ -13,10 +15,23 @@
     public class QuoteCreateOrUpdateDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(300, MinimumLength = 1)]
         public string Quote { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Character { get; set; }
+
+        // Already validated manually in service layer
+        // via ResolveEpisodeId()
         public int? EpisodeId { get; set; }
+
+        [StringLength(100)]
         public string? EpisodeTitle { get; set; }
+
+        [StringLength(20)]
         public string? EpisodeSeason { get; set; }
     }
 }
