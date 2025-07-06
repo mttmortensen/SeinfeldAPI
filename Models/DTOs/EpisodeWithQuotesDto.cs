972a25533e406e8f60c414fb76c282ec45812d9b
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-05 23:57:40 -0600

    Comments on the last Dto

--- Models/DTOs/EpisodeWithQuotesDto.cs
+++ Models/DTOs/EpisodeWithQuotesDto.cs
@@ -1,5 +1,12 @@
 ﻿namespace SeinfeldAPI.Models.DTOs
 {
+    /*
+     * This Dto is used for any GET or POST request logic 
+     * The QuoteInlineDto is to help with the nasty rat's nest with GET reqs
+     * And it allows for simple adding of quotes in POST reqs 
+     * All is needed for adding a quote within a episode is
+     * The Quote itself and who said it
+     */
     public class EpisodeWithQuotesDto
     {
         public int Id { get; set; }
