d39cad6a2ac1b52242c5a60a8654bce89a503449
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-06 10:35:46 -0600

    This Dto is now for only updating Quotes

---
+++ Models/DTOs/QuoteUpdateDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeinfeldAPI.Models.DTOs
+{
+    /*
+     * PUT /api/episodequotes
+     * 
+     * This Dto is to be used when 
+     * Updating an EpisodeQuote.
+     * By using this Dto, we can allow for optional values
+     * So that not each value needs to be updated
+     */
+
+    public class QuoteUpdateDto
+    {
+        public int Id { get; set; }
+
+        [StringLength(300)]
+        public string? Quote { get; set; }
+
+        [StringLength(100)]
+        public string? Character { get; set; }
+
+        public int? EpisodeId { get; set; }
+
+        [StringLength(100)]
+        public string? EpisodeTitle { get; set; }
+
+        [StringLength(20)]
+        public string? EpisodeSeason { get; set; }
+    }
+
+}
