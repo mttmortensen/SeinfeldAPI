8b0fcbc4f82769037bca94f9beaaf3dbea870c37
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 11:19:49 -0600

    I needed to add a custm attr that would make sure the character is one of the four main characters

---
+++ Services/Attributes/ValidMainCharacterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeinfeldAPI.Services.Attributes
+{
+    public class ValidMainCharacterAttribute  : ValidationAttribute
+    {
+        private static readonly string[] MainCharacters =
+        {
+            "Jerry", "George", "Elanie", "Kramer"
+        };
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var character = value as string;
+
+            if (string.IsNullOrWhiteSpace(character))
+                return new ValidationResult("Character name is required. ");
+
+            if(!MainCharacters.Contains(character))
+                return new ValidationResult($"'{character}' is not a main character. Allowed: {string.Join(", ", MainCharacters)}");
+
+            return ValidationResult.Success;
+        }
+    }
+}
