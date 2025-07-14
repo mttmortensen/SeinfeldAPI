128faa7818b29593dc0aed446f02d71b35676a2b
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 15:11:25 -0600

    starting on interface repo for Users

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SeinfeldAPI.Models;
+
+namespace SeinfeldAPI.Interfaces
+{
+    public interface IUserRepository
+    {
+        List<User> GetAllUsers();
+        User GetUserById(int id);
+        User GetUserByUsername(string username);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+
+    }
+}
---
+++ Repo/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SeinfeldAPI.Data;
+using SeinfeldAPI.Interfaces;
+
+namespace SeinfeldAPI.Repo
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SeinfeldDbContext _context;
+
+        public UserRepository(SeinfeldDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
