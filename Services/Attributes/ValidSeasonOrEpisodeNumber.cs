66cbc12967b7634843ecf9ab2d0c4da3d4d17498
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 11:35:17 -0600

    Logic for checking season or episode number custom attr

--- Services/Attributes/ValidSeasonOrEpisodeNumber.cs
+++ Services/Attributes/ValidSeasonOrEpisodeNumber.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace SeinfeldAPI.Services.Attributes
 {
@@ -11,6 +12,26 @@ namespace SeinfeldAPI.Services.Attributes
         public ValidSeasonOrEpisodeNumber(ValidationType type)
         {
             _type = type;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var str = value as string;
+            if (string.IsNullOrWhiteSpace(str))
+                return new ValidationResult($"{_type} is required.");
+
+            if(_type == ValidationType.Season) 
+            {
+                if (!Regex.IsMatch(str, @"^S[1-9]$"))
+                    return new ValidationResult("Season must be in the format 'S1' to 'S9' with no leading zeros.");
+            }
+            else if (_type == ValidationType.EpisodeNumber)
+            {
+                if (!Regex.IsMatch(str, @"^E([1-9]|[1-2][0-9]|30)$"))
+                    return new ValidationResult("EpisodeNumber must be in the format 'E1' to 'E30' with no leading zeros.");
+            }
+
+            return ValidationResult.Success;
         }
     }
 }
