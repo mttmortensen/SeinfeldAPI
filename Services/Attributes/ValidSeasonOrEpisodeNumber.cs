3c865169a886d8ab95e345224223e9a0ec61acc7
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 11:26:23 -0600

    I need another custom attr for making sure the episodes and seasons are done correctly. This will replace all the written out reg ex  I had on these previously

---
+++ Services/Attributes/ValidSeasonOrEpisodeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeinfeldAPI.Services.Attributes
+{
+    public class ValidSeasonOrEpisodeNumber : ValidationAttribute
+    {
+        public enum ValidationType { Season, EpisodeNumber }
+
+        private readonly ValidationType _type;
+
+        public ValidSeasonOrEpisodeNumber(ValidationType type)
+        {
+            _type = type;
+        }
+    }
+}
