17b9828f8c8864755ca2d101ecbf3a90693a8740
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 20:10:42 -0600

    decided to go with a jwt approach for auth vs the token-session based I did for the kc api.

---
+++ Services/Security/JwtHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SeinfeldAPI.Services.Security
+{
+    public class JwtHelper
+    {
+        private readonly IConfiguration _config;
+
+        public JwtHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(string username) { }
+    }
+}
