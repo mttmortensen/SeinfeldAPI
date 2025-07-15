edc1b557696e3f447717d88a1d91c2223022bb43
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 22:29:01 -0600

    adding attributes to dtos for login and register

--- Models/DTOs/LoginRequestDto.cs
+++ Models/DTOs/LoginRequestDto.cs
@@ -1,8 +1,13 @@
-﻿namespace SeinfeldAPI.Models.DTOs
+﻿using SeinfeldAPI.Services.Attributes;
+
+namespace SeinfeldAPI.Models.DTOs
 {
     public class LoginRequestDto
     {
+        [ValidUsername]
         public string Username { get; set; }
+
+        [ValidPassword]
         public string Password { get; set; }
     }
 }
