e7c1ad1812bb20d70479a08241c818e56278a633
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-14 20:29:32 -0600

    di for user repo and jwt help

--- Services/Core/AuthService.cs
+++ Services/Core/AuthService.cs
@@ -1,9 +1,19 @@
 ﻿using SeinfeldAPI.Interfaces;
+using SeinfeldAPI.Services.Security;
 
 namespace SeinfeldAPI.Services.Core
 {
     public class AuthService : IAuthService
     {
+        private readonly IUserRepository _userRepo;
+        private readonly JwtHelper _jwtHelper;
+
+        public AuthService(IUserRepository userRepo, JwtHelper jwtHelper)
+        {
+            _userRepo = userRepo;
+            _jwtHelper = jwtHelper;
+        }
+
         public bool Register(string username, string password) 
         {
 
